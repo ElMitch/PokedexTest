f04a9e412267f25d3d9e6d4a8f80add805667504
Mitch Samaniego <mitch.sam7@gmail.com>
2022-12-29 12:37:01 -0600

    feat: add type searcher

--- PokedexMitch/PokedexMitch/PrincipalView/Model/PokemonModel.swift
+++ PokedexMitch/PokedexMitch/PrincipalView/Model/PokemonModel.swift
@@ -26,3 +26,71 @@ struct PokemonsForType: Codable, Hashable {
 struct PokemonForType: Codable, Hashable {
     let pokemon: PokemonModel
 }
+
+enum TypeCells: Int, CaseIterable {
+    case normal
+    case fighting
+    case flying
+    case poison
+    case ground
+    case rock
+    case bug
+    case ghost
+    case steel
+    case fire
+    case water
+    case grass
+    case electric
+    case psychic
+    case ice
+    case dragon
+    case dark
+    case fairy
+    case unknown
+    case shadow
+
+    var typeString: String {
+        switch self {
+        case .normal:
+            return "normal"
+        case .fighting:
+            return "fighting"
+        case .flying:
+            return "flying"
+        case .poison:
+            return "poison"
+        case .ground:
+            return "ground"
+        case .rock:
+            return "rock"
+        case .bug:
+            return "bug"
+        case .ghost:
+            return "ghost"
+        case .steel:
+            return "steel"
+        case .fire:
+            return "fire"
+        case .water:
+            return "water"
+        case .grass:
+            return "grass"
+        case .electric:
+            return "electric"
+        case .psychic:
+            return "psychic"
+        case .ice:
+            return "ice"
+        case .dragon:
+            return "dragon"
+        case .dark:
+            return "dark"
+        case .fairy:
+            return "fairy"
+        case .unknown:
+            return "unknown"
+        case .shadow:
+            return "shadow"
+        }
+    }
+}
--- PokedexMitch/PokedexMitch/PrincipalView/View/PrincipalViewController.swift
+++ PokedexMitch/PokedexMitch/PrincipalView/View/PrincipalViewController.swift
@@ -15,8 +15,15 @@ class PrincipalViewController: UIViewController {
     private let viewModel = PrincipalViewModel()
     
     // UI
+    private let buttonView = UIView()
+    private let cleanButton = UIButton()
     private var collectionView: UICollectionView!
     private lazy var searchController = UISearchController()
+    private let containerTypesView = UIView()
+    private var containerTypesViewHeightContraint: NSLayoutConstraint!
+    private let tableViewForTypes = UITableView()
+
+    //// Empty View
     private let emptyView = UIView()
     private let emptyImage = UIImageView()
     private let emptyLabel = UILabel()
@@ -27,6 +34,7 @@ class PrincipalViewController: UIViewController {
     private var filteredPokemons: [PokemonModel] = []
     private var isHaveNext: Bool = true
     private var isSearchActive: Bool = false
+    private var isShowTypes: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,17 +47,42 @@ class PrincipalViewController: UIViewController {
     private func setupView() {
         view.backgroundColor = .black.withAlphaComponent(0.7)
         emptyView.backgroundColor = .black
-        setupCollectionView()
+        setupCollectionAndTable()
+
+        buttonView.registerView(cleanButton)
+
+        let principalStackView = UIStackView(arrangedSubviews: [containerTypesView, buttonView, collectionView])
+        principalStackView.axis = .vertical
+        principalStackView.distribution = .fill
+        principalStackView.spacing = 10
+        principalStackView.alignment = .fill
+
+        view.registerView(principalStackView)
+
+        containerTypesView.registerView(tableViewForTypes)
         
         view.registerView(emptyView)
         emptyView.registerView(emptyImage)
         emptyView.registerView(emptyLabel)
+
+        containerTypesViewHeightContraint = containerTypesView.heightAnchor.constraint(equalToConstant: 0)
+        containerTypesViewHeightContraint.isActive = true
         
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            principalStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            principalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            principalStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            principalStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+            tableViewForTypes.topAnchor.constraint(equalTo: containerTypesView.topAnchor, constant: 12),
+            tableViewForTypes.leadingAnchor.constraint(equalTo: containerTypesView.leadingAnchor, constant: 24),
+            tableViewForTypes.trailingAnchor.constraint(equalTo: containerTypesView.trailingAnchor, constant: -24),
+            tableViewForTypes.bottomAnchor.constraint(equalTo: containerTypesView.bottomAnchor, constant: -12),
+
+            cleanButton.heightAnchor.constraint(equalToConstant: 30),
+            cleanButton.trailingAnchor.constraint(equalTo: buttonView.trailingAnchor, constant: -24),
+            cleanButton.topAnchor.constraint(equalTo: buttonView.topAnchor),
+            cleanButton.bottomAnchor.constraint(equalTo: buttonView.bottomAnchor),
             
             emptyView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             emptyView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -74,9 +107,20 @@ class PrincipalViewController: UIViewController {
         
         //Observer Notification
         NotificationCenter.default.addObserver(self, selector: #selector(handleNotification(_:)), name: Notification.Name(NotificationKeys().typeOfPokemonNotification), object: nil)
+
+        // Set Button To Clean
+        buttonView.isHidden = true
+        cleanButton.addTarget(self, action: #selector(cleanSearchOrFiltered), for: .touchUpInside)
+        cleanButton.setAttributedTitle(.init(string: "Limpiar", attributes: NSAttributedString.pokemonDetailNumber), for: .normal)
+
+        // Set Button To Search For Type
+        let barButton = UIBarButtonItem(title: "Buscar por Tipo", style: .done, target: self, action: #selector(searchForType))
+        navigationItem.rightBarButtonItem = barButton
     }
     
-    private func setupCollectionView() {
+    // Set Components
+
+    private func setupCollectionAndTable() {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.scrollDirection = .vertical
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
@@ -84,8 +128,32 @@ class PrincipalViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.register(PokemonCell.self, forCellWithReuseIdentifier: PokemonCell.identifier)
         collectionView.backgroundColor = .clear
-        view.registerView(collectionView)
+
+        tableViewForTypes.separatorStyle = .none
+        tableViewForTypes.delegate = self
+        tableViewForTypes.dataSource = self
+        tableViewForTypes.backgroundColor = .clear
     }
+
+    private func manageSearchBarController() {
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.barStyle = .black
+        searchController.searchBar.delegate = self
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.searchBarStyle = .minimal
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.isTranslucent = true
+
+        navigationController?.navigationBar.isTranslucent = true
+
+        // Add searchController to navigation bar
+        navigationItem.searchController = searchController
+        searchController.searchBar.placeholder = "Buscar un pokemon por nombre o ID"
+    
+    }
+
+    // Get Info
     
     private func getData() {
         return viewModel.getPokemonList(offset: pokemonsList.count)
@@ -114,16 +182,6 @@ class PrincipalViewController: UIViewController {
             }.disposed(by: disposeBag)
     }
     
-    @objc private func handleNotification(_ notification: NSNotification) {
-        if let dict = notification.userInfo as NSDictionary?,
-           let type = dict["type"] as? String {
-            isSearchActive = true
-            searchController.searchBar.isHidden = true
-            filteredPokemons.removeAll()
-            getForType(with: type)
-        }
-    }
-    
     private func getForType(with type: String) {
         return viewModel.getPokemonsForType(with: type)
             .subscribe(on: MainScheduler.instance)
@@ -134,11 +192,44 @@ class PrincipalViewController: UIViewController {
                 self.filteredPokemons.append(contentsOf: pokemonForType)
                 self.reloadCollectionView()
                 self.collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+                self.buttonView.isHidden = false
                 self.emptyView.isHidden = true
             } onError: { [weak self] errorResult in
                 self?.emptyView.isHidden = false
                 print(errorResult.localizedDescription)
             }.disposed(by: disposeBag)
+    }
+
+    // Helpers
+    
+    @objc private func handleNotification(_ notification: NSNotification) {
+        if let dict = notification.userInfo as NSDictionary?,
+           let type = dict["type"] as? String {
+            isSearchActive = true
+            searchController.searchBar.isHidden = true
+            navigationItem.rightBarButtonItem?.isHidden = true
+            filteredPokemons.removeAll()
+            getForType(with: type)
+        }
+    }
+
+    @objc private func cleanSearchOrFiltered() {
+        isSearchActive = false
+        searchController.searchBar.isHidden = false
+        navigationItem.rightBarButtonItem?.isHidden = false
+        filteredPokemons.removeAll()
+        collectionView.reloadData()
+        collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+        buttonView.isHidden = true
+    }
+
+    @objc private func searchForType() {
+        isShowTypes.toggle()
+        searchController.searchBar.isHidden = isShowTypes
+        UIView.animate(withDuration: 1) {
+            self.containerTypesViewHeightContraint.constant = self.isShowTypes ? 200 : 0
+            self.view.layoutIfNeeded()
+        }
     }
 
     private func getPokemons(of list: PokemonsForType) -> [PokemonModel] {
@@ -153,25 +244,54 @@ class PrincipalViewController: UIViewController {
             self.collectionView.reloadData()
         }
     }
+}
 
-    private func manageSearchBarController() {
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = true
-        searchController.delegate = self
-        searchController.searchBar.delegate = self
-        searchController.searchBar.barStyle = .black
-        searchController.searchBar.placeholder = "Buscar un pokemon por nombre o ID"
+extension PrincipalViewController: UISearchControllerDelegate, UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        isSearchActive = true
+        guard let text = searchBar.text else { return }
+        filteredPokemons.removeAll()
+        getFilteredData(with: text.lowercased())
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearchActive = false
+        filteredPokemons.removeAll()
+        collectionView.reloadData()
+        emptyView.isHidden = true
     }
 }
 
-extension PrincipalViewController: UICollectionViewDelegate {
+extension PrincipalViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        TypeCells.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.attributedText = .init(string: TypeCells(rawValue: indexPath.row)?.typeString.capitalized ?? "", attributes: NSAttributedString.pokemonDetailNumber)
+        cell.preservesSuperviewLayoutMargins = false
+        cell.selectionStyle = .none
+        cell.layoutMargins = UIEdgeInsets(top: 0, left: 24, bottom: 0, right: 0)
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        isSearchActive = true
+        searchForType()
+        searchController.searchBar.isHidden = true
+        filteredPokemons.removeAll()
+        getForType(with: "\(indexPath.row + 1)")
+    }
+}
+
+extension PrincipalViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let id = (isSearchActive ? filteredPokemons[indexPath.row].id : indexPath.row + 1) ?? 1
         router.navigateToDetailView(sending: id)
     }
-}
 
-extension PrincipalViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         isSearchActive ? filteredPokemons.count : pokemonsList.count
     }
@@ -189,25 +309,9 @@ extension PrincipalViewController: UICollectionViewDataSource {
             getData()
         }
     }
-}
 
-extension PrincipalViewController: UICollectionViewDelegateFlowLayout {
-    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width = collectionView.bounds.width / 2 - 10
         return CGSize(width: width, height: 210)
-    }
-}
-
-extension PrincipalViewController: UISearchControllerDelegate, UISearchBarDelegate {
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        isSearchActive = true
-        guard let text = searchBar.text else { return }
-        getFilteredData(with: text.lowercased())
-    }
-
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        isSearchActive = false
-        filteredPokemons.removeAll()
-        collectionView.reloadData()
     }
 }
